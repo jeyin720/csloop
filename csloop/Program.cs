b625a3ee7778ebf4ce173a3b85886ee395e9de5b
Park Yejin <s2018s30@e-mirim.hs.kr>
2020-05-12 11:38:15 +0900

    break

--- csloop/Program.cs
+++ csloop/Program.cs
@@ -23,6 +23,15 @@ namespace csloop
             {
                 Console.WriteLine(item);
             }
+            while(true)
+            {
+                Console.Write("숫자를 입력해주세요(짝수를 입력하면 종료):");
+                int input = int.Parse(Console.ReadLine());
+                if(input%2==0)
+                {
+                    break;
+                }
+            }
         }
     }
 }
